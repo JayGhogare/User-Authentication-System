cd890090c281e275179095987c35bf28642ae511
Jay Ghogare <ghogarejay01@gmail.com>
2026-06-21 13:58:43 +0530

    Add files via upload

---
+++ User_Authentication_System.py
@@ -0,0 +1,11 @@
+User = "Jay"
+Pin = 2008 
+
+Username = input("Enter Username:")
+Password = int(input("Enter Password:"))
+
+if Username == User and Pin == Password:
+    print("Login Succesfull")
+else :
+    print("Username or Password is incorrect")    
+    
